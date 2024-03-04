765cb1aba40cb2b3370aae88c894e93b2bbf8c3c
ArthurOnWeb <Arthur.rattanavong@etu.univ-savoie.fr>
2024-03-04 17:40:15 +0100

    [FEATURE] : 4 endpoint pour le backend

--- ProjetChatBack/app.py
+++ ProjetChatBack/app.py
@@ -17,20 +17,19 @@ PORT = int(os.environ.get('PORT', 3000))
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/info834'
 mongo = PyMongo(app)
 client = MongoClient('mongodb://localhost:27017/')
-db = client['votre_base_de_donnees']
+db = client['info834']
 collection_conversations = db['conversations']
 # Connect to Redis
 redis_client = redis.StrictRedis(host='localhost', port=6379, decode_responses=True)
 
 # Endpoint pour vérifier l'authentification
-# Endpoint pour vérifier l'authentification
 @app.route('/authentification', methods=['POST'])
 def verifier_authentification():
-    data = request.json
-    if data:
+    utilisateur_data = request.json
+    if utilisateur_data:
         # Récupérer les informations d'authentification
-        email = data.get('email')
-        mot_de_passe = data.get('mot_de_passe')
+        email = utilisateur_data.get('email')
+        mot_de_passe = utilisateur_data.get('mot_de_passe')
 
         # Vérifier les informations d'authentification dans MongoDB
         utilisateur = mongo.db.utilisateurs.find_one({'email': email, 'mot_de_passe': mot_de_passe})
@@ -43,10 +42,25 @@ def verifier_authentification():
     else:
         return jsonify({'error': 'Les données d\'authentification sont requises'}), 400
 
-
-# Endpoint pour créer un utilisateur
  
-
+# Endpoint pour créer un utilisateur
+@app.route('/utilisateurs', methods=['POST'])
+def creer_utilisateur():
+    utilisateur_data = request.json
+    if utilisateur_data:
+        # Vérifier si l'utilisateur existe déjà dans la base de données
+        utilisateur_existant = mongo.db.utilisateurs.find_one({"email": utilisateur_data["email"]})
+        if utilisateur_existant:
+            return jsonify({'message': 'Cet utilisateur existe déjà', 'id': str(utilisateur_existant["_id"])}), 400
+        else:
+            # Insérer l'utilisateur dans la base de données MongoDB
+            result = mongo.db.utilisateurs.insert_one(utilisateur_data)
+            # Retourner l'ID de l'utilisateur créé
+            return jsonify({'message': 'Utilisateur créé avec succès', 'id': str(result.inserted_id)}), 201
+    else:
+        return jsonify({'error': 'Les données de l\'utilisateur sont requises'}), 400
+    
+    
 # Endpoint pour envoyer un message
 @app.route('/chats/<conversation_id>/messages', methods=['POST'])
 def envoyer_message(conversation_id):
--- ProjetChatBack/test.py
+++ ProjetChatBack/test.py
@@ -7,91 +7,78 @@ Created on Thu Feb 22 10:40:26 2024
 
 import requests
 import json
-'''
-# Données d'authentification à envoyer
-auth_data = {
-    "email": "alice@example.com",
-    "mot_de_passe": "motdepasse123"
-}
+def authentification():
+    # Données d'authentification à envoyer
+    auth_data = {
+        "email": "alice@example.com",
+        "mot_de_passe": "motdepasse123"
+    }
 
-# URL de l'endpoint d'authentification
-url = 'http://localhost:3000/authentification'
+    # URL de l'endpoint d'authentification
+    url = 'http://localhost:3000/authentification'
 
-# Envoi de la requête POST avec les données d'authentification
-response = requests.post(url, json=auth_data)
+    # Envoi de la requête POST avec les données d'authentification
+    response = requests.post(url, json=auth_data)
 
-# Affichage de la réponse
-print(response.json())
-
-
-
-
-# Données de l'utilisateur à envoyer
-utilisateur_data = {
-    "nom": "Alice",
-    "email": "alice@example.com",
-    "mot_de_passe": "motdepasse123"
-}
-
-# URL de l'endpoint
-url = 'http://localhost:3000/utilisateurs'
-
-# Envoi de la requête POST avec les données JSON de l'utilisateur
-response = requests.post(url, json=utilisateur_data)
+    # Affichage de la réponse
+    print(response.json())
 
 
 
+def create_user(): 
+    # Données de l'utilisateur à envoyer
+    utilisateur_data = {
+        "nom": "Alice",
+        "email": "alice@example.com",
+        "mot_de_passe": "motdepasse123"
+    }
 
-# Données du message à envoyer
-message_data = {
-    "expediteur": {"_id": "65d716d6bf13b296b7109749", "nom": "Alice"},
-    "contenu": "weshhh",
-    "timestamp": "2024-02-28T19:40:00Z"
-}
+    # URL de l'endpoint
+    url = 'http://localhost:3000/utilisateurs'
 
-# ID de la conversation
-conversation_id = "conversation1"
-
-# URL de l'endpoint
-url = f'http://localhost:3000/chats/{conversation_id}/messages'
-
-# Envoi de la requête POST avec les données JSON du message
-response = requests.post(url, json=message_data)
-
+    # Envoi de la requête POST avec les données JSON de l'utilisateur
+    response = requests.post(url, json=utilisateur_data)
+    # Affichage de la réponse
+    print(response.json())
 
 
 
+def send_message():
+    # Données du message à envoyer
+    message_data = {
+        "expediteur": {"_id": "65d716d6bf13b296b7109749", "nom": "Alice"},
+        "contenu": "weshhh",
+        "timestamp": "2024-02-28T19:40:00Z"
+    }
 
-# Données de la conversation à envoyer
-conversation_data = {
-    "nom": "Conversation entre Alice et Bob",
-    "participants": ["Alice", "Bob"]
-}
+    # ID de la conversation
+    conversation_id = "65e5f84b1294cfe0ff3088b5"
 
-# URL de l'endpoint
-url = 'http://localhost:3000/chats'
+    # URL de l'endpoint
+    url = f'http://localhost:3000/chats/{conversation_id}/messages'
 
-# Envoi de la requête POST avec les données JSON de la conversation
-response = requests.post(url, json=conversation_data)
+    # Envoi de la requête POST avec les données JSON du message
+    response = requests.post(url, json=message_data)
+    # Affichage de la réponse
+    print(response.json())
 
-# Affichage de la réponse
-print(response.json())
 
-'''
 
-# Données des utilisateurs à envoyer
-utilisateurs_data = [
-    {
-        "_id": "1",
-        "nom": "Tom",
-        "email": "tom@example.com",
-        "mot_de_passe": "tompasse123"
-    },
-    
-]
+def create_conversation():
+    # Données de la conversation à envoyer
+    conversation_data = {
+        "nom": "Conversation entre Alice et Bob",
+        "participants": ["Alice", "Bob"]
+    }
 
-# URL de l'endpoint
-url = 'http://localhost:3000/utilisateurs'
+    # URL de l'endpoint
+    url = 'http://localhost:3000/chats'
 
-# Envoi de la requête POST avec les données JSON des utilisateurs
-response = requests.post(url, json=utilisateurs_data)
+    # Envoi de la requête POST avec les données JSON de la conversation
+    response = requests.post(url, json=conversation_data)
+
+    # Affichage de la réponse
+    print(response.json())
+
+if __name__ == "__main__":
+    send_message()
