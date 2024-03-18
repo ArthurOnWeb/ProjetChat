425d37e605b9c8b5698c85344f813ed53555278e
ArthurOnWeb <Arthur.rattanavong@etu.univ-savoie.fr>
2024-03-18 12:46:00 +0100

    [FEATURE] : accès à la page

--- ProjetChatFront/src/app/app.routes.ts
+++ ProjetChatFront/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ChatPageComponent } from './chat-page/chat-page.component';
 
 export const routes: Routes = [
     
     {path : 'login', component: LoginComponent},
     {path : 'sign-up', component: SignUpComponent},
+    {path : 'chat-page', component: ChatPageComponent},
     {path: '', redirectTo: 'login', pathMatch:'full'},
 ];
